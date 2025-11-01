55a1a6f88a8f6e60b2df98c3a317e63ef2d2f042
silviu85 <roman.ilie.silviu@gmail.com>
2025-11-01 19:15:29 +0200

    security updates

---
+++ run_dev.bat
@@ -0,0 +1,15 @@
+@echo off
+REM A script to start Dev suite.
+
+echo Starting testing environment...
+SET COMPOSE_CMD=docker-compose -f docker-compose.yml --env-file .env
+
+%COMPOSE_CMD% up -d
+echo.
+cd ../ai-chat-react
+echo.
+echo Starting React development server...
+%COMPOSE_CMD% up -d
+cd ../ai-php-api
+echo.
+echo Dev environment is up and running...
